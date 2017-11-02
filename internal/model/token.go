b3eab71c4b87504b8957417a8ad6c8798a696189
Muhammad Alfan Miftakhul Hudha <muhammadalfan.mh@gmail.com>
2017-11-02 15:21:26 +0700

    update session lifetime

--- internal/model/token.go
+++ internal/model/token.go
@@ -36,6 +36,10 @@ func GenerateToken(u User) Token {
 		c.Close()
 		panic(err)
 	}
+
+	//if _, err := c.Do("EXPIRE" ,"TOKENS"+u.Account.Id+u.ID, TOKENLIFE) ;err != nil {
+	//	panic(err)
+	//}
 	c.Close()
 
 	setSession(u, t)
@@ -63,6 +67,10 @@ func setSession(u User, token Token) {
 		c.Close()
 		panic(err)
 	}
+
+	if _, err := c.Do("EXPIRE" ,"SESSION"+token.Token, TOKENLIFE * 60) ;err != nil {
+		panic(err)
+	}
 }
 
 func IsExistToken(token string) bool {
@@ -70,11 +78,17 @@ func IsExistToken(token string) bool {
 	defer c.Close()
 
 	exists, _ := redis.Bool(c.Do("EXISTS", "SESSION"+token))
-	if dt, err := GetSession(token); err != nil {
-		exists = false
-	} else if dt.ExpiredAt.Before(time.Now()) {
-		exists = false
+	if exists{
+		//update expired token
+		if _, err := c.Do("EXPIRE" ,"SESSION"+token, TOKENLIFE * 60) ;err != nil {
+			panic(err)
+		}
 	}
+	//if dt, err := GetSession(token); err != nil {
+	//	exists = false
+	//} else if dt.ExpiredAt.Before(time.Now()) {
+	//	exists = false
+	//}
 
 	c.Close()
 	return bool(exists)
@@ -161,130 +175,3 @@ func DeleteSession(u User) {
 		panic(err)
 	}
 }
-
-//FORGOT PASSWORD TOKEN
-//
-// func GenerateForgotPasswordToken(AccountID, userId string) Token {
-// 	DeleteForgotPasswordSession(AccountID, userId)
-//
-// 	r := getNewTokenString()
-// 	now := time.Now()
-//
-// 	t := Token{
-// 		Token:     r,
-// 		ExpiredAt: now.Add(time.Duration(TOKENLIFE) * time.Minute),
-// 	}
-//
-// 	c := redisPool.Get()
-// 	defer c.Close()
-//
-// 	if _, err := c.Do("SET", "FORGOT_PASSWORD_TOKENS"+AccountID+userId, t.Token); err != nil {
-// 		c.Close()
-// 		panic(err)
-// 	}
-// 	c.Close()
-//
-// 	setForgotPasswordSession(AccountID, userId, t)
-//
-// 	return t
-// }
-
-// func setForgotPasswordSession(AccountID string, userId string, token Token) {
-// 	c := redisPool.Get()
-// 	defer c.Close()
-//
-// 	sd := SessionData{
-// 		UserId:    userId,
-// 		AccountID: AccountID,
-// 		ExpiredAt: token.ExpiredAt,
-// 	}
-//
-// 	json, _ := json.Marshal(sd)
-//
-// 	if _, err := c.Do("DEL", "FORGOT_PASSWORD_SESSION"+token.Token); err != nil {
-// 		c.Close()
-// 		panic(err)
-// 	}
-//
-// 	if _, err := c.Do("SET", "FORGOT_PASSWORD_SESSION"+token.Token, json); err != nil {
-// 		c.Close()
-// 		panic(err)
-// 	}
-// }
-//
-// func AuthenticateForgotPasswordToken(token string) error {
-// 	if !isExistForgotPasswordToken(token) {
-// 		fmt.Println("err 1")
-// 		return ErrTokenNotFound
-// 	}
-//
-// 	sd, err := GetForgotPasswordSession(token)
-// 	if err != nil {
-// 		fmt.Println("err 2")
-// 		return ErrTokenNotFound
-// 	}
-//
-// 	if sd.ExpiredAt.Before(time.Now()) {
-// 		fmt.Println("err 3")
-// 		return ErrTokenExpired
-// 	}
-//
-// 	return nil
-// }
-//
-// func isExistForgotPasswordToken(token string) bool {
-// 	c := redisPool.Get()
-// 	defer c.Close()
-//
-// 	exists, _ := redis.Bool(c.Do("EXISTS", "FORGOT_PASSWORD_SESSION"+token))
-//
-// 	c.Close()
-// 	return bool(exists)
-// }
-//
-// func getForgotPasswordToken(AccountID, userId string) (string, error) {
-// 	c := redisPool.Get()
-// 	defer c.Close()
-// 	t, err := redis.String(c.Do("GET", "FORGOT_PASSWORD_TOKENS"+AccountID+userId))
-// 	if err != nil {
-// 		c.Close()
-// 		return "", ErrTokenNotFound
-// 	}
-// 	c.Close()
-//
-// 	return t, nil
-// }
-//
-// func GetForgotPasswordSession(token string) (SessionData, error) {
-// 	c := redisPool.Get()
-// 	defer c.Close()
-// 	t, err := redis.String(c.Do("GET", "FORGOT_PASSWORD_SESSION"+token))
-// 	if err != nil {
-// 		c.Close()
-// 		return SessionData{}, ErrTokenNotFound
-// 	}
-// 	c.Close()
-//
-// 	var data SessionData
-// 	if err := json.Unmarshal([]byte(t), &data); err != nil {
-// 		panic(err)
-// 	}
-// 	return data, nil
-// }
-//
-// func DeleteForgotPasswordSession(AccountID, userId string) {
-// 	t, _ := getForgotPasswordToken(AccountID, userId)
-//
-// 	c := redisPool.Get()
-// 	defer c.Close()
-//
-// 	if _, err := c.Do("DEL", "FORGOT_PASSWORD_SESSION"+t); err != nil {
-// 		c.Close()
-// 		panic(err)
-// 	}
-//
-// 	if _, err := c.Do("DEL", "FORGOT_PASSWORD_TOKENS"+AccountID+userId); err != nil {
-// 		c.Close()
-// 		panic(err)
-// 	}
-// }
