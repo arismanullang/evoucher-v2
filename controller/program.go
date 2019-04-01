34cbc95856fc142919ffc43f84e9a1801477cb12
Muhammad Alfan Miftakhul Hudha <muhammadalfan.mh@gmail.com>
2019-04-01 14:12:42 +0700

    new partner module

---
+++ controller/program.go
@@ -0,0 +1,105 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gilkor/evoucher/model"
+	u "github.com/gilkor/evoucher/util"
+	"github.com/go-zoo/bone"
+)
+
+//PostProgram : POST partner data
+func PostProgram(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+	qp := u.NewQueryParam(r)
+
+	var program *model.Program
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&program); err != nil {
+		res.SetError(JSONErrFatal.SetMessage(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+	// TO-DO
+	//validate partners(?)
+	for _, v := range program.Partners {
+		_, _, err := model.GetPartnerByID(qp, v.ID)
+		if err != nil {
+			res.SetError(JSONErrBadRequest.SetMessage(err.Error()))
+			res.JSON(w, res, JSONErrBadRequest.Status)
+			return
+		}
+	}
+	//insert program -> partners
+	fmt.Println(program)
+	if err := program.Insert(); err != nil {
+		fmt.Println(err)
+		res.SetError(JSONErrFatal.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+
+	// generate voucher
+
+	res.SetResponse(program)
+	res.JSON(w, res, http.StatusCreated)
+}
+
+// GetProgram :
+func GetProgram(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+	programs, next, err := model.GetPrograms(qp)
+	if err != nil {
+		res.SetError(JSONErrBadRequest.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrBadRequest.Status)
+		return
+	}
+
+	res.SetResponse(programs)
+	res.SetPagination(r, qp.Page, next)
+	res.JSON(w, res, http.StatusOK)
+}
+
+//GetProgramByID :
+func GetProgramByID(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+	id := bone.GetValue(r, "id")
+	program, err := model.GetProgramByID(id, qp)
+	if err != nil {
+		res.SetError(JSONErrBadRequest.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrBadRequest.Status)
+		return
+	}
+
+	res.SetResponse(program)
+	res.JSON(w, res, http.StatusOK)
+}
+
+// DeleteProgram :
+func DeleteProgram(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+	id := bone.GetValue(r, "id")
+	fmt.Println("program id ", id)
+	program, err := model.GetProgramByID(id, qp)
+	if err != nil {
+		res.SetError(JSONErrBadRequest.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrBadRequest.Status)
+		return
+	}
+	// Delete program
+	fmt.Println("delete prog ", program)
+	if err := program.Delete(); err != nil {
+		res.SetError(JSONErrFatal.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+	res.JSON(w, res, http.StatusCreated)
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/gilkor/evoucher
 go 1.12
 
 require (
+	github.com/davidrjenni/reftools v0.0.0-20180914123528-654d0ba4f96d // indirect
 	github.com/gilkor/athena v0.0.0-20190304115447-10789df036dd
 	github.com/go-zoo/bone v0.0.0-20190117145001-d7ce1372afa7
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.0.0
+	github.com/prometheus/prometheus v2.5.0+incompatible // indirect
 	github.com/urfave/negroni v1.0.0
 	google.golang.org/appengine v1.5.0 // indirect
 )
