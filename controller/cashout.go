93216c6dd3a5dc3737705990686cc9b70ec1a558
Aris Manullang <arismanullang0@gmail.com>
2020-05-29 15:21:37 +0700

    Cashout

--- controller/cashout.go
+++ controller/cashout.go
@@ -1,1 +1,130 @@
 package controller
+
+import (
+	"encoding/json"
+	"github.com/gilkor/evoucher-v2/model"
+	u "github.com/gilkor/evoucher-v2/util"
+	"github.com/go-zoo/bone"
+	"github.com/gorilla/schema"
+	"net/http"
+	"time"
+)
+
+type CashoutFilter struct {
+	ID            string     `schema:"id" filter:"array"`
+	AccountID     string     `schema:"account_id" filter:"string"`
+	Code          string     `schema:"code" filter:"string"`
+	PartnerID     string     `schema:"partner_id" filter:"string"`
+	BankAccount   string     `schema:"bank_account" filter:"string"`
+	Amount        float64    `schema:"amount" filter:"string"`
+	PaymentMethod string     `schema:"payment_method" filter:"string"`
+	CreatedAt     *time.Time `schema:"created_at" filter:"string"`
+	CreatedBy     string     `schema:"created_by" filter:"string"`
+	UpdatedAt     *time.Time `schema:"updated_at" filter:"date"`
+	UpdatedBy     string     `schema:"updated_by" filter:"date"`
+	Status        string     `schema:"status" filter:"enum"`
+}
+
+//GetCashouts : GET list of Cashouts
+func GetCashouts(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+
+	var decoder = schema.NewDecoder()
+	decoder.IgnoreUnknownKeys(true)
+
+	var f CashoutFilter
+	if err := decoder.Decode(&f, r.Form); err != nil {
+		res.SetError(JSONErrFatal.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+
+	qp.SetFilterModel(f)
+
+	Cashouts, next, err := model.GetCashouts(qp)
+	if err != nil {
+		res.SetError(JSONErrFatal.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+
+	res.SetResponse(Cashouts)
+	res.SetNewPagination(r, qp.Page, next, (*Cashouts)[0].Count)
+	res.JSON(w, res, http.StatusOK)
+}
+
+//GetCashoutSummary : GET list of Cashout Summary
+func GetCashoutSummary(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+
+	cashoutSummary, next, err := model.GetCashoutSummary(qp)
+	if err != nil {
+		res.SetError(JSONErrFatal.SetArgs(err.Error()))
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+
+	res.SetResponse(r)
+	res.SetNewPagination(r, qp.Page, next, cashoutSummary[0].Count)
+	res.JSON(w, res, http.StatusOK)
+}
+
+//GetCashoutByID : GET
+func GetCashoutByID(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	qp := u.NewQueryParam(r)
+	id := bone.GetValue(r, "id")
+	Cashout, _, err := model.GetCashoutByID(id, qp)
+	if err != nil {
+		res.SetError(JSONErrResourceNotFound)
+		res.JSON(w, res, JSONErrResourceNotFound.Status)
+		return
+	}
+
+	res.SetResponse(Cashout)
+	res.JSON(w, res, http.StatusOK)
+}
+
+//DeleteCashout : remove Cashout
+func DeleteCashout(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	id := bone.GetValue(r, "id")
+	p := model.Cashout{ID: id}
+	if err := p.Delete(); err != nil {
+		res.SetError(JSONErrResourceNotFound)
+		res.JSON(w, res, JSONErrResourceNotFound.Status)
+		return
+	}
+	res.JSON(w, res, http.StatusOK)
+}
+
+//PostCashoutByPartner : POST Cashout by partner
+func PostCashoutByPartner(w http.ResponseWriter, r *http.Request) {
+	res := u.NewResponse()
+
+	var req model.Cashout
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&req); err != nil {
+		u.DEBUG(err)
+		res.SetErrorWithDetail(JSONErrFatal, err)
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+	// reqCashout.ID = bone.GetValue(r, "holder")
+	response, err := req.Insert()
+	if err != nil {
+		u.DEBUG(err)
+		res.SetErrorWithDetail(JSONErrFatal, err)
+		res.JSON(w, res, JSONErrFatal.Status)
+		return
+	}
+
+	res.SetResponse(response)
+	res.JSON(w, res, http.StatusCreated)
+}
--- model/cashout.go
+++ model/cashout.go
@@ -1,1 +1,249 @@
 package model
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gilkor/evoucher-v2/util"
+)
+
+type (
+	CashoutSummary struct {
+		Date          *time.Time `db:"date" json:"date,omitempty"`
+		PartnerID     string     `db:"partner_id" json:"partner_id,omitempty"`
+		UnpaidAmount  float64    `db:"unpaid_amount" json:"unpaid_amount,omitempty"`
+		CashoutAmount float64    `db:"cashout_amount" json:"cashout_amount,omitempty"`
+		VoucherQty    int        `db:"voucher_qty" json:"voucher_aty,omitempty"`
+		Count         int        `db:"count" json:"-"`
+	}
+	//Cashout : represent of cashout table model
+	Cashout struct {
+		ID             string         `db:"id" json:"id,omitempty"`
+		AccountID      string         `db:"account_id" json:"account_id,omitempty"`
+		Code           string         `db:"code" json:"code,omitempty"`
+		PartnerID      string         `db:"partner_id" json:"partner_id,omitempty"`
+		BankAccount    string         `db:"bank_account" json:"bank_account,omitempty"`
+		Amount         float64        `db:"amount" json:"amount,omitempty"`
+		PaymentMethod  string         `db:"payment_method" json:"payment_method,omitempty"`
+		CreatedAt      *time.Time     `db:"created_at" json:"created_at,omitempty"`
+		CreatedBy      string         `db:"created_by" json:"created_by,omitempty"`
+		UpdatedAt      *time.Time     `db:"updated_at" json:"updated_at,omitempty"`
+		UpdatedBy      string         `db:"updated_by" json:"updated_by,omitempty"`
+		Status         string         `db:"status" json:"status,omitempty"`
+		CashoutDetails CashoutDetails `json:"cashout_details,omitempty"`
+		Count          int            `db:"count" json:"-"`
+	}
+	Cashouts      []Cashout
+	CashoutDetail struct {
+		ID        int        `db:"id" json:"id,omitempty"`
+		CashoutID string     `db:"cashout_id" json:"cashout_id,omitempty"`
+		VoucherID string     `db:"voucher_id" json:"voucher_id,omitempty"`
+		CreatedAt *time.Time `db:"created_at" json:"created_at,omitempty"`
+		CreatedBy string     `db:"created_by" json:"created_by,omitempty"`
+		UpdatedAt *time.Time `db:"updated_at" json:"updated_at,omitempty"`
+		UpdatedBy string     `db:"updated_by" json:"updated_by,omitempty"`
+		Status    string     `db:"status" json:"status,omitempty"`
+	}
+	CashoutDetails []CashoutDetail
+)
+
+// GetCashoutByID :  get list Cashouts by ID
+func GetCashoutByID(id string, qp *util.QueryParam) (*Cashouts, bool, error) {
+	return getCashouts(qp, "id", id)
+}
+
+// GetCashouts : list Cashout
+func GetCashouts(qp *util.QueryParam) (*Cashouts, bool, error) {
+	return getCashouts(qp, "1", "1")
+}
+
+func getCashouts(qp *util.QueryParam, key, value string) (*Cashouts, bool, error) {
+	q, err := qp.GetQueryByDefaultStruct(Cashout{})
+	if err != nil {
+		return &Cashouts{}, false, err
+	}
+	q += `
+			FROM
+				cashout
+			WHERE 
+				status = ?			
+			AND ` + key + ` = ?`
+
+	q += qp.GetQuerySort()
+	q += qp.GetQueryLimit()
+	fmt.Println(q)
+	var resd Cashouts
+	err = db.Select(&resd, db.Rebind(q), StatusCreated, value)
+	if err != nil {
+		return &Cashouts{}, false, err
+	}
+
+	next := false
+	if len(resd) > qp.Count {
+		next = true
+	}
+	if len(resd) < qp.Count {
+		qp.Count = len(resd)
+	}
+
+	return &resd, next, nil
+}
+
+// GetCashouts : list Cashout Summary
+func GetCashoutSummary(qp *util.QueryParam) ([]CashoutSummary, bool, error) {
+	return getCashoutSummary(qp, "1", "1")
+}
+
+func getCashoutSummary(qp *util.QueryParam, key, value string) ([]CashoutSummary, bool, error) {
+	q, err := qp.GetQueryByDefaultStruct(CashoutSummary{})
+	if err != nil {
+		return []CashoutSummary{}, false, err
+	}
+	q += `
+			FROM
+				v_cashout_summary
+			WHERE 
+				status = ?			
+			AND ` + key + ` = ?`
+
+	q += qp.GetQuerySort()
+	q += qp.GetQueryLimit()
+	fmt.Println(q)
+	var resd []CashoutSummary
+	err = db.Select(&resd, db.Rebind(q), StatusCreated, value)
+	if err != nil {
+		return []CashoutSummary{}, false, err
+	}
+
+	next := false
+	if len(resd) > qp.Count {
+		next = true
+	}
+	if len(resd) < qp.Count {
+		qp.Count = len(resd)
+	}
+
+	return resd, next, nil
+}
+
+//Insert : single row inset into table
+func (c *Cashout) Insert() (*[]Cashout, error) {
+	tx, err := db.Beginx()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	q := `INSERT INTO 
+				cashouts 
+				( 
+					account_id
+					, code 
+					, partner_id 
+					, bank_account
+					, amount 
+					, payment_method
+					, created_by
+					, updated_by
+					, status
+				)
+			VALUES 
+				( ?, ?, ?, ?, ?, ?, ?, ?)
+			RETURNING
+				id
+				, account_id
+				, code 
+				, partner_id 
+				, bank_account
+				, amount 
+				, payment_method
+				, created_by
+				, updated_by
+				, status
+	`
+
+	var res []Cashout
+	err = tx.Select(&res, tx.Rebind(q), c.AccountID, c.Code, c.PartnerID, c.BankAccount, c.Amount, c.PaymentMethod, c.CreatedBy, c.UpdatedBy, StatusCreated)
+	if err != nil {
+		return nil, err
+	}
+
+	var resd []CashoutDetail
+	for _, cd := range c.CashoutDetails {
+		q = `INSERT INTO 
+			cashout_details (cashout_id, voucher_id, created_by, updated_by, status)
+			VALUES (?, ?, ?, ?, ?)
+			RETURNING
+				id, cashout_id, voucher_id, created_by, updated_by, status
+	`
+		err = tx.Select(&res, tx.Rebind(q), cd.CashoutID, cd.VoucherID, cd.CreatedBy, cd.UpdatedBy, StatusCreated)
+		if err != nil {
+			return nil, err
+		}
+	}
+	res[0].CashoutDetails = resd
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+//Delete : soft delated data by updateting row status to "deleted"
+func (c *Cashout) Delete() error {
+	tx, err := db.Beginx()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	q := `UPDATE
+				cashout 
+			SET
+				deleted_at = now(),
+				deleted_by = ?
+				status = ?			
+			WHERE 
+				id = ?	
+			RETURNING
+				id
+				, account_id
+				, code 
+				, partner_id 
+				, bank_account
+				, amount 
+				, payment_method
+				, created_by
+				, updated_by
+				, status
+	`
+	var res []Cashout
+	err = tx.Select(&res, tx.Rebind(q), c.UpdatedBy, StatusDeleted, c.ID)
+
+	q = `UPDATE
+				cashouts_details
+			SET
+				deleted_at = now(),
+				deleted_by = ?
+				status = ?			
+			WHERE 
+				cashout_id = ?	
+			RETURNING
+				id
+	`
+	var resd []CashoutDetail
+	err = tx.Select(&res, tx.Rebind(q), c.UpdatedBy, StatusDeleted, c.ID)
+
+	res[0].CashoutDetails = resd
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
